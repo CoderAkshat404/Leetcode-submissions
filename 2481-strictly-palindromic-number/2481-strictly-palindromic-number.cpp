69f0569654938d7d6d310f13fe3639ed2afd2998
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-08-11 12:26:58 +0530

    Time: 3 ms (18.85%), Space: 7.7 MB (6.54%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool check(int n, int i) {
-        string s = "";
-        while (n != 0) {
-            int x = n % i;
-            n = n / i;
-            string v = to_string(x);
-            s += v;
+    bool check(int n,int i){
+        string s="";
+        while(n!=0){
+            int x=n%i;
+            n=n/i;
+            string v=to_string(x);
+            s+=v;
         }
-        int p = 0;
-        int j = s.length() - 1;
-        while (p <= j) {
-            if (s[p] != s[j]) {
+        int p=0;
+        int j=s.length()-1;
+        while(p<=j){
+            if(s[p]!=s[j]){
                 return false;
+               
+
             }
-            p++; // Increment p
-            j--; // Decrement j
+            p++;
+            j--;
         }
         return true;
     }
-
     bool isStrictlyPalindromic(int n) {
-        for (int i = 2; i <= n - 2; i++) {
-            bool ans = check(n, i);
-            if (!ans) {
+        for(int i=2;i<=n-2;i++){
+            bool ans=check(n,i);
+            if(ans==false){
                 return false;
+                break;
             }
         }
         return true;
+        
     }
 };
