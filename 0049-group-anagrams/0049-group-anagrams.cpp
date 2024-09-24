af40e7f975052b6bec1360f594211c7dad813830
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-09-24 09:30:35 +0530

    Time: 551 ms (5.04%), Space: 24.5 MB (66.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, bool> m;
+        vector<string> a=strs;
+        for(int i=0;i<a.size();i++){
+            sort(a[i].begin(),a[i].end());
+        }
+        unordered_map<string,bool> m;
         vector<vector<string>> ans;
-        
-        for (int i = 0; i < strs.size(); i++) {
-            if (m[strs[i]] == true) {
+        for(int i=0;i<strs.size();i++){
+            if(m[strs[i]]==true){
                 continue;
             }
             vector<string> v;
-            
-            for (int j = 0; j < strs.size(); j++) {
-                bool flag = true;
-                
-                if (strs[j].length() != strs[i].length()) {
-                    flag = false;
-                } else {
-                    for (int p = 0; p < strs[j].length(); p++) {
-                        if (count(strs[j].begin(), strs[j].end(), strs[j][p]) !=
-                            count(strs[i].begin(), strs[i].end(), strs[j][p])) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                }
-                
-                if (flag) {
+            for(int j=0;j<strs.size();j++){
+                if(a[i]==a[j]){
                     v.push_back(strs[j]);
-                    m[strs[j]] = true;
+                    m[strs[j]]=true;
                 }
+
             }
             ans.push_back(v);
         }
