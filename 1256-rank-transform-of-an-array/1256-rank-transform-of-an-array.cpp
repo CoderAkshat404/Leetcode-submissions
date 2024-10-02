c4567cd3cfa306018f3863b22fecd53f3ce0ac1c
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-10-02 12:01:40 +0530

    Time: 64 ms (74.36%), Space: 43.2 MB (59.66%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> a=arr;
-        sort(a.begin(),a.end());
-        unordered_map<int,int> m;
-        unordered_map<int,bool> mp;
-        int rank=1;
-        for(int i=0;i<a.size();i++){
-            if(mp[a[i]]==false){
-                m[a[i]]=rank;
-                mp[a[i]]=true;
-                rank++;
-            }
-
-        }
-        for(auto i:m){
-            cout<<i.first<<" "<<i.second<<endl;
-        }
-        for(int i=0;i<arr.size();i++){
-            arr[i]=m[arr[i]];
-        }
-        return arr;        
+        vector<int> b = arr;
+sort(b.begin(),b.end());
+unordered_map<int,int> rank;
+int r = 1;
+for(int i=0;i<b.size();i++){
+if(rank[b[i]] == 0){
+rank[b[i]] = r++;
+}
+}
+for(int i=0;i<arr.size();i++){
+arr[i] = rank[arr[i]];
+}
+return arr;     
     }
 };
