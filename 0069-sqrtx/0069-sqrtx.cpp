a3d4d0fea9efe2bc695e293d703121c016f4037d
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-12-07 16:19:26 +0530

    Time: 0 ms (100%), Space: 8.4 MB (14.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int n=sqrt(x);
-        return n;
+        long long int  l=-1;
+        long long int  r=INT_MAX/2;
+        while(r-l>1){
+            int m = l + (r - l) / 2; 
+            if((long long )m*m<=x){
+                l=m;
+            }
+            else{
+                r=m;
+            }
+        }
+        return l;
         
     }
 };
