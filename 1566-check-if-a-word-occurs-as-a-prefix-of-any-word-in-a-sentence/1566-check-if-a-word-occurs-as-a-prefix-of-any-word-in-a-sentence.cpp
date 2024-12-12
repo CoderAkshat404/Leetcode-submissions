5ab66b28f26fa61ada520531290f1601eaae91ed
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-12-12 23:14:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.45%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        vector<string> s;
-        string p="";
+        vector<string> words;
+        string w = "";
+        int wordcount = 0;
         for(int i=0;i<sentence.length();i++){
-            while(sentence[i]!=' ' && i<sentence.length()){
-                p+=sentence[i];
-                i++;
-            }
-            s.push_back(p);
-            p="";
-        }
-        s.push_back(p);
-        // for(auto i:s){
-        //     cout<<i<<endl;
-        // }
-        int ans=-1;
-        int c=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i].find(searchWord)!=string::npos){
-                for(int j=0;j<searchWord.length();j++){
-                    if(s[i][j]==searchWord[j]){
-                        c++;
-                    }
+            if(sentence[i]==' '){
+                wordcount++;
+                if(w.find(searchWord)==0){
+                    return wordcount;
                 }
-                if(c==searchWord.length()){
-                    return i+1;
-                }
-                c=0;
+                w="";
+            }else{
+                w+=sentence[i];
             }
         }
-        return ans;
-        
+        wordcount++;
+        if(w.find(searchWord)==0){
+            return wordcount;
+        }
+        return -1;
     }
 };
