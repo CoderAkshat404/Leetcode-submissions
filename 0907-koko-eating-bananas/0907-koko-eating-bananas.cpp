3e3c34ee79b5191c54dafb8ca806b687a5bceb56
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2024-12-28 12:44:04 +0530

    Time: 12 ms (37%), Space: 22.9 MB (41.7%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,38 +1,36 @@
-#include <vector>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
-    bool check(int m, vector<int>& piles, int h) {
-        int hours = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            // Use integer arithmetic to avoid precision issues
-            hours += (piles[i] + m - 1) / m;
-            if (hours > h) return false; // Early exit if we exceed allowed hours
+    int check(int m,vector<int>& piles, int h){
+        int l=0;
+        for(int i=0;i<piles.size();i++){
+            l+=ceil((double)piles[i]/m);
         }
-        return hours <= h;
+        if(l<=h){
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            maxi = max(maxi, piles[i]);
+        int maxi=INT_MAX;
+        for(int i=0;i<piles.size();i++){
+            maxi=max(maxi,piles[i]);
         }
-        
-        int lo = 1;
-        int hi = maxi;
-        int ans = -1;
-
-        while (lo <= hi) {
-            int m = lo + (hi - lo) / 2;
-            if (check(m, piles, h)) {
-                ans = m;
-                hi = m - 1; // Try for a smaller speed
-            } else {
-                lo = m + 1; // Try for a larger speed
+        int lo=1;
+        int hi=maxi;
+        int ans=-1;
+        while(lo<=hi){
+            int m=hi-(hi-lo)/2;
+            if(check(m,piles,h)==1){
+                ans=m;
+                hi=m-1;
+            }
+            else{
+                lo=m+1;
             }
         }
         return ans;
+        
     }
 };
