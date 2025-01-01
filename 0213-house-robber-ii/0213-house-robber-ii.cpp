946c0e67e8689abf41860bb90f5fdce62bd4803b
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-01-01 13:24:57 +0530

    Time: 21 ms (1.02%), Space: 11.1 MB (7.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-     int solve(vector<int>& nums,int index,vector<vector<int>>&dp,bool& flag){
+     int solve(vector<int>& nums,int index,vector<vector<int>> & dp,int flag){
+        cout<<index<<" "<<flag<<endl;
         if(index>=nums.size()){
             return 0;
         }
-        if(index==nums.size()-1 && flag==false){
+        if(index==nums.size()-1 && flag==0){
             return nums[index];
         }
-        if(index==nums.size()-1 && flag==true){
+        if(index==nums.size()-1 && flag==1){
             return 0;
         }
         if(dp[index][flag]!=-1){
             return dp[index][flag];
         }
+        int include = 0,exclude = 0;
         if(index==0){
-            flag=true;
+            include = nums[index]+solve(nums,index+2,dp,true);
+            exclude = solve(nums,index+1,dp,false);
+        }else{
+            include = nums[index]+solve(nums,index+2,dp,flag);
+            exclude = solve(nums,index+1,dp,flag);
         }
-        int include=solve(nums,index+2,dp,flag)+nums[index];
-        if(index==0){
-            flag=false;
-        }
-        int exclude=solve(nums,index+1,dp,flag);
         dp[index][flag]=max(include,exclude);
         return dp[index][flag];
     }
     int rob(vector<int>& nums) {
-        bool flag=false;
+        // bool flag=false;
         vector<vector<int>> dp(nums.size(),vector<int>(2,-1));
-        int ans=solve(nums,0,dp,flag);
+        int ans=solve(nums,0,dp,0);
         return ans;
     }
 };
