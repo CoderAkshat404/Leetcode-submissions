afc2347e99b64c8e2f4c85befc0cbf79713fc8b5
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-01-15 08:07:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (5.14%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int count1 = 0;
+        int ans = 0;
+
+        // Count the number of '1' bits in num2
+        for (int i = 0; i < 32; i++) {
+            int b = (num2 >> i) & 1;
+            if (b == 1) {
+                count1++;
+            }
+        }
+
+        int count2 = 0;
+        vector<int> bits;
+
+        // Extract bits of num1 and count '1's
+        for (int i = 31; i >= 0; i--) {
+            int b = (num1 >> i) & 1;
+            if (b == 1) {
+                count2++;
+            }
+            bits.push_back(b);
+        }
+
+        if (count1 <= count2) {
+            int c = 0;
+
+            // Set bits from num1 to ans until count1 is reached
+            for (int i = 0; i < bits.size(); i++) {
+                if (bits[i] == 1) {
+                    ans += (1 << (31 - i)); // Corrected bit position calculation
+                    c++;
+                    if (c == count1) {
+                        break;
+                    }
+                }
+            }
+
+        } else {
+            int c = 0;
+
+            // Set all '1' bits from num1 to ans
+            for (int i = 0; i < bits.size(); i++) {
+                if (bits[i] == 1) {
+                    ans += (1 << (31 - i)); // Corrected bit position calculation
+                    c++;
+                }
+            }
+
+            // Add additional bits to ans until count1 is reached
+            for (int i = bits.size() - 1; i >= 0; i--) {
+                if (bits[i] == 0) {
+                    ans += (1 << (31 - i)); // Corrected bit position calculation
+                    c++;
+                    if (c == count1) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
