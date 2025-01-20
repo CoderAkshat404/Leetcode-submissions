ce7e95780034d76bbb6e0d0fa47e4c915eafe42d
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-01-20 18:27:03 +0530

    Time: 2 ms (92.58%), Space: 10.3 MB (95.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -21,30 +21,30 @@ public:
         
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
         int n=word1.length();
         int m=word2.length();
+        vector<int> forw(m+1,0),curr(m+1,0);
+        
         for(int j=0;j<=m;j++){
-            dp[n][j]=m-j;
+            forw[j]=m-j;
 
         }
-        for(int i=0;i<=n;i++){
-            dp[i][m]=n-i;
-        }
         for(int i=n-1;i>=0;i--){
+            curr[m]=n-i;
             for(int j=m-1;j>=0;j--){
                 if(word1[i]==word2[j]){
-                    dp[i][j]=dp[i+1][j+1];
+                   curr[j]=forw[j+1];
                 }
                 else{
-                    int op1=1+dp[i+1][j];
-                    int op2=1+dp[i+1][j+1];
-                    int op3=1+dp[i][j+1];
-                    dp[i][j]=min(op1,min(op2,op3));
+                    int op1=1+forw[j];
+                    int op2=1+forw[j+1];
+                    int op3=1+curr[j+1];
+                    curr[j]=min(op1,min(op2,op3));
                 }
             }
+            forw=curr;
         }
-        return dp[0][0];
+        return forw[0];
         
         
     }
