2fd20bf51ab4305cf3a7b7de81a041acd3cdb237
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-01-22 17:10:09 +0530

    Time: 10 ms (5.98%), Space: 22.1 MB (11.66%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findans(int i,int buy,vector<int>& prices, vector<vector<int>>&dp){
+        if(i>=prices.size()){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        int ans=0;
+        if(buy){
+            ans=max(-prices[i]+findans(i+1,0,prices,dp),findans(i+1,1,prices,dp));
+
+        }
+        else{
+            ans=max(prices[i]+findans(i+1,1,prices,dp),findans(i+1,0,prices,dp));
+        }
+        return dp[i][buy]=ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+        int ans=findans(0,1,prices,dp);
+        return ans;
+        
+    }
+};
