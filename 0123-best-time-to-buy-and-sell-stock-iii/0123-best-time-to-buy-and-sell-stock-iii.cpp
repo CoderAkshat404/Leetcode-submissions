130bcc00201ba2765a8d0692ff0226966a3e68ad
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-01-23 10:12:03 +0530

    Time: 404 ms (41.55%), Space: 211.5 MB (38.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -22,9 +22,28 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        int ans=findans(0,1,2,prices,dp);
-        return ans;
-        
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j][0]=0;
+            }
+
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k=1;k<3;k++){
+                    int ans=0;
+                    if(j==1){
+                        ans=max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }
+                    else{
+                        ans=max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
+                    dp[i][j][k]=ans;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
