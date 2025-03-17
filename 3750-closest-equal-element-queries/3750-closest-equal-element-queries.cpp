5cfd1ed6c20fbd62714295b22ffafd4ee200b8c7
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-03-17 17:43:50 +0530

    Time: 900 ms (0%), Space: 216.9 MB (100%) - LeetHub

--- 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        unordered_map<int, vector<int>> m;
-        int n = nums.size();
-
-        // Store the indices for each number
-        for (int i = 0; i < n; i++) {
+        unordered_map<int,vector<int>> m;
+        for(int i=0;i<nums.size();i++){
             m[nums[i]].push_back(i);
         }
-
         vector<int> ans;
-        for (int y : queries) {
-            int x = nums[y];
-            vector<int>& v = m[x]; 
+        for(int i=0;i<queries.size();i++){
+            int x=nums[queries[i]];
+            vector<int>&v=m[x];
+            int y=queries[i];
+            int n=v.size();
             if(v.size()==1){
                 ans.push_back(-1);
                 continue;
             }
-
-            int idx = lower_bound(v.begin(), v.end(), y) - v.begin();
-            int dist = INT_MAX;
-
-            if (idx > 0) dist = min(dist, y - v[idx - 1]); 
-            if (idx < v.size() - 1) dist = min(dist, v[idx + 1] - y); 
-           
-            if (y == v.back() || y == v[0]) {
-                dist = min(dist, (n - v.back()) + v[0]);
+            int idx=lower_bound(v.begin(),v.end(),y)-v.begin();
+            int dist=1e9;
+            if(idx!=0){
+                int a=v[idx-1];
+                dist=y-a;
             }
+            if(idx!=n-1){
+                int b=v[idx+1];
+                dist=min(dist,b-y);
 
-            if (dist == INT_MAX) dist = -1;
+            }
+            int p=nums.size()-v.back();
+            int q=v[0];
+            // cout<<q<<endl;
+            // cout<<p+q<<endl;
+            cout<<y<<endl;
+            if(y==v.back() || y==v[0]){
+                dist=min(dist,p+q);
+
+            }
             ans.push_back(dist);
         }
-        
         return ans;
+        
     }
 };
