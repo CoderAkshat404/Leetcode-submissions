3005d3eb394665f894c4aee6e86929ed7c3c7455
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-03-18 12:56:38 +0530

    Time: 1737 ms (5.04%), Space: 61 MB (64.15%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,70 +1,64 @@
 class Solution {
 public:
-    int check(int & m,vector<int>& nums){
+    bool check(vector<int>& nums,int len){
         vector<int> v(32,0);
-        for(int i=0;i<m;i++){
-            int x=nums[i];
+        for(int i=0;i<len;i++){
             for(int j=0;j<32;j++){
-                int b=(x>>j)&1;
-                if(b==1){
+                if(((1<<j) & nums[i])!=0){
                     v[j]++;
                 }
             }
         }
-        bool flag=true;
-        for(auto i:v){
-            if(i>1){
-                flag=false;
+        bool poss = true;
+        for(int i=0;i<32;i++){
+            if(v[i]>1){
+                poss = false;
                 break;
             }
         }
-        if(flag) return 1;
-        int i=0;
-        int j=m-1;
+        if(poss)    return true;
+        int i=0, j = len-1;
         while(j+1<nums.size()){
-            int x=nums[j+1];
-            for(int p=0;p<32;p++){
-                int b=(x>>p)&1;
-                if(b==1){
-                    v[p]++;
+            //increment for nums[j+1]
+            for(int k=0;k<32;k++){
+                if(((1<<k)&nums[j+1])!=0){
+                    v[k]++;
                 }
             }
-            x=nums[i];
-            for(int p=0;p<32;p++){
-                int b=(x>>p)&1;
-                if(b==1){
-                    v[p]--;
+            //decrementing for nums[i];
+            for(int k=0;k<32;k++){
+                if(((1<<k)&nums[i])!=0){
+                    v[k]--;
                 }
             }
-            bool flag=true;
-            for(auto i:v){
-            if(i>1){
-                flag=false;
-                break;
+            //checking
+            poss = true;
+            for(int k=0;k<32;k++){
+                if(v[k]>1){
+                    poss = false;
+                    break;
                 }
             }
-             if(flag) return 1;
-             i++;
-             j++;
+            if(poss)    return true;  
 
+            i++;
+            j++;  
         }
-        return 0;
+        return false;
     }
     int longestNiceSubarray(vector<int>& nums) {
-        int lo=1;
-        int hi=nums.size();
-        int ans=-1;
-        while(lo<=hi){
-            int m=lo+(hi-lo)/2;
-            if(check(m,nums)){
-                lo=m+1;
-                ans=m;
-            }
-            else{
-                hi=m-1;
+        int s = 1, e = nums.size(), ans = 1;
+        while(s<=e){
+            int len = s + (e-s)/2;
+            if(check(nums,len)){
+                // cout<<len<<" true"<<endl;
+                ans = len;
+                s= len+1;
+            }else{
+                // cout<<len<<" false\n";
+                e = len-1;
             }
         }
         return ans;
-        
     }
 };
