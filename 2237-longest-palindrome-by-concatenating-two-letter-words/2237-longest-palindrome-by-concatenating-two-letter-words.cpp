6f6f0df14561061cbd2901ae6e03f36f2727c2fc
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-05-25 12:01:35 +0530

    Time: 35 ms (77.37%), Space: 172 MB (44.24%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -5,39 +5,33 @@ public:
         for(auto i:words){
             m[i]++;
         }
-        // Debug print - can remove in final version
-        // for(auto i:m){
-        //     cout<<i.first<<" "<<i.second<<endl;
-        // }
-
         int best=0;
         int counter=0;
         int best2=0;
         unordered_map<string,bool> mp;
-        
         for(auto i:m){
             string s=i.first;
             string a=s;
             reverse(a.begin(),a.end());
-            if(a==s){  // Palindromic word
+            if(s==a){
                 if(i.second%2==0){
                     best2+=i.second;
                 }
-                else if(i.second&1){
+                else{
                     best2+=(i.second-1);
-                    best++;  // Only one central word needed
+                    best++;
                 }
             }
             else if(mp.find(a)==mp.end() && m.count(a)){
                 counter+=min(i.second,m[a]);
-                mp[s]=true;  // Mark both s and a as visited
+                mp[s]=true;
                 mp[a]=true;
             }
         }
-
-        // Debug print - can remove in final version
-        // cout<<counter<<" "<<best2<<" "<<best;
+        cout<<counter<<" "<<best2<<" "<<best;
         
-        return (4*counter + 2*best2 + (best>0 ? 2 : 0));
+        return (4*counter+(2*min(best,1)+2*best2));
+
+        
     }
 };
