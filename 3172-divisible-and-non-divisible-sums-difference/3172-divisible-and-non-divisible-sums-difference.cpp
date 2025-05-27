fa18fe724985a83123540221667be62f3af3640f
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-05-27 11:47:42 +0530

    Time: 0 ms (100%), Space: 8.3 MB (94.16%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int x=n/m;
+        int num2=m*(x*(x+1))/2;
+        int num1=n*(n+1)/2;
+        return num1-2*num2;
+        
+    }
+};
