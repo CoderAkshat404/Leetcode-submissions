abfd92c183366b5ed4e93007277a571588a288cf
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-01 14:34:01 +0530

    Time: 0 ms (100%), Space: 23.4 MB (91.82%) - LeetHub

---
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool checkEqualPartitions(vector<int>& nums, long long target) {
+        double pro=1;
+        for(int i=0;i<nums.size();i++){
+            pro*=(nums[i]);
+
+        }
+        return sqrtl(pro)==sqrtl((double)(target)*target);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ;
+        
+    }
+};
