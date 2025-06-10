330d870fafd32fa9db27e2a130eb52302952f0c2
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-10 15:01:57 +0530

    Time: 0 ms (100%), Space: 13.5 MB (5.78%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int findans(vector<vector<int>>& triangle,vector<vector<int>> &dp,int i,int j){
-        if(i>=triangle.size() && j>=triangle[i].size()){
-            return INT_MAX;
-        }
-        if(i==triangle.size()-1){
-            return triangle[i][j];
-        }
-        if(j>=triangle[i].size()){
-            return INT_MAX;
-        }
-        if(dp[i][j]!=-INT_MAX){
-            return dp[i][j];
-        }
-        dp[i][j]=min((triangle[i][j]+findans(triangle,dp,i+1,j)),(triangle[i][j]+findans(triangle,dp,i+1,j+1)));
-        return dp[i][j];
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp;
-        for(int i=0;i<triangle.size();i++){
-            int x=i+1;
-            vector<int> v(x,-INT_MAX);
-            dp.push_back(v);
+        int n=triangle.size();
+        int m=triangle.back().size();
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,1e8));
+        for(int i=0;i<m;i++){
+            dp[n-1][i]=triangle[n-1][i];
         }
-
-        int ans=findans(triangle,dp,0,0);
-        return ans;
+        for(int i=n-2;i>=0;i--){
+            int x=triangle[i].size();
+            for(int j=0;j<x;j++){
+                dp[i][j]=triangle[i][j]+min(dp[i+1][j+1],dp[i+1][j]);
+                
+            }
+        }
+        return dp[0][0];
         
     }
 };
