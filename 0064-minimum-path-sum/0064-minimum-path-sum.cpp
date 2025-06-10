f7a2dd03b054206f89cd4486c0bc612f17de14c8
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-10 14:45:24 +0530

    Time: 12 ms (6.07%), Space: 16.4 MB (38.33%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    int rec(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& grid, int n, int m) {
-        // Base case: If we reach the bottom-right corner, return the value
-        if (i == n - 1 && j == m - 1) {
-            return grid[i][j];
-        }
-        // If out of bounds, return a large number
-        else if (i >= n || j >= m) {
-            return INT_MAX;
-        }
-
-        // If the value is already computed, return it
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Recursively find the minimum path sum
-        dp[i][j] = grid[i][j] + min(rec(i + 1, j, dp, grid, n, m), rec(i, j + 1, dp, grid, n, m));
-        return dp[i][j];
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        // Initialize DP table with -1
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,1e8));
+        dp[n-1][m-1]=grid[n-1][m-1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    continue;
+                }
+                dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
 
-        // Start recursion from the top-left corner (0, 0)
-        return rec(0, 0, dp, grid, n, m);
+            }
+        }
+        return dp[0][0];
+        
     }
 };
