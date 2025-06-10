afc9b61198e3a9afa8138752830dd46446d6ccd3
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-10 14:38:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (71.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums[0].size(),0));
+        vector<vector<long long>> dp(nums.size()+1,vector<long long>(nums[0].size()+1,0));
         int n=nums.size();
         int m=nums[0].size();
         if(nums[n-1][m-1]==1){
             return 0;
         }
-        for(int i=0;i<n;i++){
-            if(nums[i][m-1]){
-                dp[i][m-1]=0;
-
-
-            }
-            else{
-            dp[i][m-1]=1;}
-
-        }
-        for(int i=0;i<n;i++){
-            if(dp[n-1][i]==1){
-                dp[n-1][i]=0;
-
-            }
-            else{
-            dp[n-1][i]=1;}
-
-        }
         dp[n-1][m-1]=1;
-        for(int i=n-2;i>=0;i--){
-            for(int j=m-2;j>=0;j--){
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    continue;
+                }
                 if(nums[i][j]==1){
                     dp[i][j]=0;
                 }
