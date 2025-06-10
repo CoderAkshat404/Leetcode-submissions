600e6bbda11365bc5a3fc352eee0bb9c72efcc4f
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-10 13:05:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    int findans(int i, int j, vector<vector<int>>& dp, int m, int n, vector<vector<int>>& grid) {
-        if(i>=m || j>=n){
+    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
+        vector<vector<int>> dp(nums.size(),vector<int>(nums[0].size(),0));
+        int n=nums.size();
+        int m=nums[0].size();
+        if(nums[n-1][m-1]==1){
             return 0;
         }
-         if(grid[i][j]==1){
-            return 0;
-        }
-        if (i == m-1 && j == n-1) {
-            return 1; // Reached destination
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j]; // Return cached result
-        }
+        for(int i=0;i<n;i++){
+            if(nums[i][m-1]){
+                dp[i][m-1]=0;
 
-        // Recursive calls
-        dp[i][j] = findans(i+1, j, dp, m, n, grid) + findans(i, j+1, dp, m, n, grid);
-        return dp[i][j];
-    }
 
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+            }
+            else{
+            dp[i][m-1]=1;}
+
+        }
+        for(int i=0;i<n;i++){
+            if(dp[n-1][i]==1){
+                dp[n-1][i]=0;
+
+            }
+            else{
+            dp[n-1][i]=1;}
+
+        }
+        dp[n-1][m-1]=1;
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+                if(nums[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else{
+                    dp[i][j]=dp[i][j+1]+dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+
         
-        // Initialize DP table
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        // Call the helper function starting at the top-left corner
-        return findans(0, 0, dp, m, n, obstacleGrid);
     }
 };
