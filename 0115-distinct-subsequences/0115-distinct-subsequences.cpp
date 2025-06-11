f77e368e557a556c6f5dc33cc2c483ce15ce073e
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-12 00:44:13 +0530

    Time: 31 ms (55.19%), Space: 44.4 MB (21.98%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        const int MOD = 1e9 + 7; // Define the modulo value
-        int n = s.length();
-        int m = t.length();
-        
-        // Use a 2D DP table to store the results
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
-        
-        // Base case: If `t` is empty, there's exactly 1 way to match it
-        for (int i = 0; i <= n; i++) {
-            dp[i][m] = 1;
+        int n=s.length();
+        int m=t.length();
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][m]=1;
         }
-        
-        // Fill the DP table bottom-up
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (s[i] == t[j]) {
-                    dp[i][j] = (dp[i + 1][j + 1] + dp[i + 1][j]) % MOD;
-                } else {
-                    dp[i][j] = dp[i + 1][j] % MOD;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s[i]==t[j]){
+                    dp[i][j]=dp[i+1][j+1]+dp[i+1][j];
+                }
+                else{
+                    dp[i][j]=dp[i+1][j];
                 }
             }
         }
+        return dp[0][0];
         
-        // Return the result modulo MOD
-        return dp[0][0] % MOD;
     }
 };
