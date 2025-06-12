a853b601334a8590ba3bd38cfe7a705a50872fac
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-12 22:27:36 +0530

    Time: 39 ms (28.23%), Space: 17.6 MB (34.98%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(),1);
-        vector<int> cnt(nums.size(),1);
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        vector<int> dp(n,1);
+        vector<int> cnt(n,1);
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[j]<nums[i] && dp[j]+1>dp[i]){
-                    dp[i]=dp[j]+1;
-                    cnt[i]=cnt[j];
-                }
-                else if(nums[j]<nums[i] && dp[j]+1==dp[i]){
-                    cnt[i]+=cnt[j];
+                if(nums[j]<nums[i]){
+                    if(dp[i]<dp[j]+1){
+                        cnt[i]=cnt[j];
+                        dp[i]=dp[j]+1;
+                    } 
+                    else if(dp[i]==dp[j]+1){
+                        cnt[i]+=cnt[j];
+                    }   
                 }
             }
-        
         }
-        unordered_map<int,int> m;
         int maxi=INT_MIN;
-        for(int i=0;i<dp.size();i++){
-            m[dp[i]]+=cnt[i]; 
-            maxi=max(maxi,dp[i]);  
+        for(auto i:dp){
+            maxi=max(maxi,i);
         }
-        if(count(dp.begin(),dp.end(),1)==nums.size()){
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxi){
+                ans+=cnt[i];
+            }
+        }
+        if(maxi==1){
             return nums.size();
         }
-        return m[maxi];
+        return ans;
+        
         
     }
 };
