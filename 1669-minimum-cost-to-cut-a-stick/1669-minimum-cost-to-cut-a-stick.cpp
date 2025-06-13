f5cf2b0d4d15d19174f6e12eff5bfc125b1305c5
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-13 14:44:02 +0530

    Time: 15 ms (92.94%), Space: 14 MB (24.69%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -21,14 +21,14 @@ public:
         vector<vector<int>> dp(n,vector<int>(n,0));
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<n;j++){
+                if(j==i+1){
+                    continue;
+                }
                 int ans=1e9;
                 for(int k=i+1;k<j;k++){
                     ans=min(ans,cuts[j]-cuts[i]+dp[i][k]+dp[k][j]);
                 }
-                if (ans != 1e9)  // only update if a valid cut was found
-            dp[i][j] = ans;
-        else
-            dp[i][j] = 0;
+                dp[i][j] = ans;
             }
         }
         return dp[0][n-1];
