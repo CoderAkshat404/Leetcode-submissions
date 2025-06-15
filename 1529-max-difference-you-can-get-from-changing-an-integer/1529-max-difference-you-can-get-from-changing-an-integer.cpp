bf94e33dfe6dfa6848abbf24531b3c9158e90327
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-15 18:00:25 +0530

    Time: 3 ms (15.17%), Space: 10.2 MB (6.16%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        int maxi = -1;
-        int mini = 1e9;
-        for(int i = '0'; i <= '9'; i++) {
-            for(int k = '0'; k <= '9'; k++) {
-                string s = to_string(num);
-                for(int j = 0; j < s.length(); j++) {
-                    if(s[j] == (char)k) {
-                        s[j] = (char)i;
-                    }
-                }
-                int n = stoi(s);
-                if(s[0] != '0') {  
-                    maxi = max(maxi, n);
-                    mini = min(mini, n);
+        
+        int maxi=-1;
+        int mini=1e9;
+        for(int i='0';i<='9';i++){
+            for(int k='0';k<='9';k++){
+            string s=to_string(num);
+            for(int j=0;j<s.length();j++){
+                if(s[j]==k){
+                    s[j]=i;
                 }
             }
+            int n=stoi(s);
+            if(s[0]!='0'){
+                maxi=max(maxi,n);
+                mini=min(mini,n);
+            }
+
+            }
         }
-        return maxi - mini;
+        return maxi-mini;
+        
     }
 };
