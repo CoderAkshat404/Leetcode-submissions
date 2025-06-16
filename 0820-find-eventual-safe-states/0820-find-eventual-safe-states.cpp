21757051ec70bf505fb6b3e0d8d9db04fd9e6925
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-16 17:21:14 +0530

    Time: 45 ms (56.99%), Space: 66 MB (53.89%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
-bool dfs(int node,vector<vector<int>> &graph,vector<int>& vis,vector<int>&pathvis,vector<int> &safe)
-    // Function to detect cycle in a directed graph.
-    {
-        vis[node]=1;
-        pathvis[node]=1;
-        vector<int> v=graph[node];
-        for(auto i:v){
-            if(!vis[i]){
-                if(dfs(i,graph,vis,pathvis,safe)){
-                    return true;
-                }
-     
-            }
-            else if(pathvis[i]){
-                return true;
-            }
-        }
-        safe[node]=1;
-        pathvis[node]=0;
-        return false;
-        
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        vector<int> safe(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-            dfs(i,graph,vis,pathvis,safe);
-                
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[graph[i][j]].push_back(i);
+                indegree[i]++;
             }
         }
         vector<int> ans;
+        queue<int> q;
         for(int i=0;i<n;i++){
-            if(safe[i]){
+            if(indegree[i]==0){
                 ans.push_back(i);
+                q.push(i);
             }
         }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto i:adj[node]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    ans.push_back(i);
+                    q.push(i);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
-        
-        // code here
-    
+
         
     }
 };
