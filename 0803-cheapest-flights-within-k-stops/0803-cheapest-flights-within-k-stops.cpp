95ce8bdd5c87ec9a152edf53e3acbdde5cd47827
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-17 19:07:29 +0530

    Time: 0 ms (100%), Space: 18 MB (66.17%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -13,19 +13,15 @@ public:
         dist[src]=0;
         q.push({0,src,0});
         while(!q.empty()){
-            vector<int> t=q.front();
+            int st=q.front()[0];
+            int node=q.front()[1];
+            int d=q.front()[2];
             q.pop();
-            int stops=t[0];
-            int node=t[1];
-            int d=t[2];
-            vector<pair<int,int>> v=adj[node];
-            for(auto i:v){
-                int nd=d+i.second;
-                if(nd<dist[i.first]){
-                    if(stops<=k){
-                        dist[i.first]=nd;
-                        q.push({stops+1,i.first,nd});
-                    }
+            for(auto i:adj[node]){
+                if(st<=k){
+                    if(dist[i.first]>d+i.second){
+                    dist[i.first]=min(d+i.second,dist[i.first]);
+                    q.push({st+1,i.first,dist[i.first]});}
                 }
             }
 
