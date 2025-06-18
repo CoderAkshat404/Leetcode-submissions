66643fdbdcddb0cc0a930925c7dcd9a9639844e3
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-18 23:06:02 +0530

    Time: 7 ms (76.38%), Space: 42.5 MB (75.84%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,85 +1,81 @@
-class DSU{
-    vector<int> rank,parent,size;
+class DSU {
     public:
-    DSU(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        size.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i; 
-            size[i]=i;
+        vector<int> rank, parent, size;
+        
+        DSU(int n) {
+            rank.resize(n + 1, 0);
+            parent.resize(n + 1);
+            size.resize(n + 1, 1);  
+            
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+            }
         }
-    }
-    int findparent(int n){
-        if(n==parent[n]){
-            return n;
+    
+        int findparent(int n) {
+            if (n == parent[n]) return n;
+            return parent[n] = findparent(parent[n]);
         }
-        return parent[n]=findparent(parent[n]);
-    }
-    void addedge(int u,int v){
-        int pu=findparent(u);
-        int pv=findparent(v);
-        if(pu==pv){
-            return;
+    
+        void addedge(int u, int v) {
+            int pu = findparent(u);
+            int pv = findparent(v);
+            if (pu == pv) return;
+    
+            if (rank[pu] > rank[pv]) {
+                parent[pv] = pu;
+            } else if (rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            } else {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
         }
-        if(rank[pu]>rank[pv]){
-            parent[pv]=pu;
+    
+        void unionbysize(int u, int v) {
+            int pu = findparent(u);
+            int pv = findparent(v);
+            if (pu == pv) return;
+    
+            if (size[pu] > size[pv]) {
+                parent[pv] = pu;
+                size[pu] += size[pv]; 
+            } else {
+                parent[pu] = pv;
+                size[pv] += size[pu]; 
+            }
         }
-        else if(rank[pu]<rank[pv]){
-            parent[pu]=pv;
-        }
-        else{
-            parent[pu]=pv;
-            rank[pv]++;
-
-        }
-    }
-    void DSUBYsize(int u,int v){
-        int pu=findparent(u);
-        int pv=findparent(v);
-        if(pu==pv){
-            return;
-        }
-        if(size[pu]>size[pv]){
-            parent[pv]=pu;
-            size[pv]+=size[pu];
-        }
-        else{
-            parent[pu]=pv;
-            size[pu]+=size[pv];
-
-        }
-
-    }
 };
+
 
 
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         DSU ds(n);
-        int extra=0;
+        int cnt=0;
         for(int i=0;i<connections.size();i++){
-            int u=connections[i][0];
-            int v=connections[i][1];
-            if(ds.findparent(u)==ds.findparent(v)){
-                extra++;
+            int v=connections[i][0];
+            int u=connections[i][1];
+            if(ds.findparent(v)!=ds.findparent(u)){
+                ds.unionbysize(u,v);
+                
             }
             else{
-                ds.addedge(u,v);
+                cnt++;
             }
         }
-        int nc=0;
+        int needed=0;
         for(int i=0;i<n;i++){
             if(ds.findparent(i)==i){
-                nc++;
+                needed++;
+
             }
         }
-        if(extra>=nc-1){
-            return nc-1;
+        if(cnt>=needed-1){
+            return needed-1;
         }
         return -1;
-
         
     }
 };
