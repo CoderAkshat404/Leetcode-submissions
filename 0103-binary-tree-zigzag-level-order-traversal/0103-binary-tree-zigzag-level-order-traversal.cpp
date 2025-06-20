1bee0a25cefbf33c06fb72eba51cc616b99e1a79
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-20 14:24:07 +0530

    Time: 0 ms (100%), Space: 15.1 MB (48.19%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -15,37 +15,35 @@ public:
         if(root==NULL){
             return {};
         }
-        queue<TreeNode*> q;
-        int count=0;
-        q.push(root);
         vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        int c=0;
         while(!q.empty()){
-            count++;
+            int n=q.size();
             vector<int> v;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                v.push_back(node->val);
+            for(int i=0;i<n;i++){
+                v.push_back(q.front()->val);
+                if(q.front()->left){
+                    q.push(q.front()->left);
+                }
+                if(q.front()->right){
+                    q.push(q.front()->right);
+                }
                 q.pop();
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-                
+
             }
-            if(count%2==0){
-                reverse(v.begin(),v.end());
+            if(c%2==0){
                 ans.push_back(v);
 
             }
             else{
+                reverse(v.begin(),v.end());
                 ans.push_back(v);
             }
-
+            c++;
         }
-        return ans;
-        
+
+     return ans;   
     }
 };
