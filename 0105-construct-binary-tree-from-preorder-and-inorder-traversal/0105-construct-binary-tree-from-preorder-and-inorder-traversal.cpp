050172180308dc9f67a51ffbc99840ca27b18b00
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-21 15:10:53 +0530

    Time: 4 ms (58.25%), Space: 27.5 MB (37.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,24 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* solve(int pres,int pree,int ins,int ine,vector<int>& preorder, vector<int>& inorder,unordered_map<int,int>&m){
-        if (pres > pree || ins > ine) {
-    return nullptr;
-}
-        TreeNode* x=new TreeNode(preorder[pres]);
-        int numsleft=m[x->val]-ins;
-        x->left=solve(pres+1,pres+numsleft,ins,m[x->val]-1,preorder,inorder,m);
-        x->right=solve(pres+numsleft+1,pree,m[x->val]+1,ine,preorder,inorder,m);
-        return x;
+    unordered_map<int,int> m;
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int instart,int inend,int prestart, int preend){
+        if(prestart>preend){
+            return NULL;
+        }
+        if(prestart==preend){
+            TreeNode* root=new TreeNode(preorder[prestart]); 
+            return root;   
+        }
+        TreeNode* root=new TreeNode(preorder[prestart]);
+        int count=m[preorder[prestart]]-instart;
+        root->left=solve(preorder,inorder,instart,m[preorder[prestart]]-1,prestart+1,prestart+count);
+        root->right=solve(preorder,inorder,m[preorder[prestart]]+1,inend,prestart+count+1,preend);
+        return root;
+
+
 
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int,int> m;
-        for(int i=0;i<inorder.size();i++){
+        for(int i=0;i<preorder.size();i++){
             m[inorder[i]]=i;
         }
-        return solve(0,preorder.size()-1,0,inorder.size()-1,preorder,inorder,m);
-
+        
+        return solve(preorder,inorder,0,preorder.size()-1,0,preorder.size()-1);
         
     }
 };
