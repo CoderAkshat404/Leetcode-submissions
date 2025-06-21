120028bec29e370d260d4b6e526d8c7bf60d4b53
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-21 10:56:10 +0530

    Time: 19 ms (31.52%), Space: 20.4 MB (8.7%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int solve(int i,int j,vector<int> &v,int k, vector<vector<int>>&dp){
-        if(i==j) return 0;
-        if(v[j]-v[i]<=k) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=min(v[i]+solve(i+1,j,v,k,dp),v[j]-v[i]-k+solve(i,j-1,v,k,dp));
+        if(i>=j){
+            return 0;
+        }
+        if(v[j]-v[i]<=k){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int op1=v[i]+solve(i+1,j,v,k,dp);
+        int op2=v[j]-(k+v[i])+solve(i,j-1,v,k,dp);
+        return dp[i][j]=min(op1,op2);
     }
     int minimumDeletions(string word, int k) {
-        unordered_map<int,int> m;
-        for(int i=0;i<word.length();i++){
-            m[word[i]]++;
+        unordered_map<char, int> m;
+        for (char c : word) {
+            m[c]++;
         }
+
         vector<int> v;
-        for(auto i:m){
+        for (auto i : m) {
             v.push_back(i.second);
         }
-        sort(v.begin(),v.end());
-        vector<vector<int>> dp(v.size(),vector<int>(v.size(),-1));
-        int ans=solve(0,v.size()-1,v,k,dp);
-        return ans;
-        
+        sort(v.begin(), v.end());
+        int n=v.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(0,v.size()-1,v,k,dp);
     }
 };
