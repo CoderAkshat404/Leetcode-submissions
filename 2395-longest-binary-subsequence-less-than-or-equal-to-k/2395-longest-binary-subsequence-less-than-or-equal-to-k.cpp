f7df3dee98f7a0a5cd0951fb723bb2c6e947386a
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-26 14:16:50 +0530

    Time: 3 ms (30.69%), Space: 9.7 MB (29.1%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,34 +1,23 @@
-#define ll  long long
 class Solution {
 public:
-    int solve(string&s,ll i,ll k,ll len,ll sum,vector<vector<ll>>&dp){
-        if(i<0){
+    int n;
+    int dp[1001];
+    int f(string &s, int k, int i) {
+        if(i == 0)
             return 0;
-        }
-        if(dp[i][len]!=-1){
-            return  dp[i][len];
-        }
-        ll ans=0;
-        if(s[i]=='1'){
-            ll take=0;
-            ll nottake=solve(s,i-1,k,len,sum,dp);
-            if(sum+pow(2,len)<=k){
-                take=1+solve(s,i-1,k,len+1,sum+pow(2,len),dp);
-            }
-            ans=max(take,nottake);
-        }
-        else{
-            ll nottake=solve(s,i-1,k,len,sum,dp);
-            ll take=1+solve(s,i-1,k,len+1,sum,dp);
-            ans=max(ans,max(take,nottake));
-
-        }
-        return  dp[i][len]=ans;
-
+        if(dp[i] != -1)
+            return dp[i];
+        // take
+        int x = 0;
+        if((s[i-1]-'0')*pow(2, n-i) <= k)
+            x = 1 + f(s, k-(s[i-1]-'0')*pow(2, n-i), i-1);
+        // not take
+        int y = f(s, k, i-1);
+        return dp[i] = max(x, y);
     }
-    int longestSubsequence(string s, int k) {
-        vector<vector<ll>> dp(s.length(),vector<ll>(s.length(),-1));
-        return solve(s,s.length()-1,k,0,0,dp);
-        
+    int longestSubsequence(string &s, int k) {
+        n = s.length();
+        memset(dp, -1, sizeof(dp));
+        return f(s, k, n);
     }
 };
