bcd42e6961f09d66117c12e1c816785f6ab2db52
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-26 16:19:51 +0530

    Time: 31 ms (22.09%), Space: 11.9 MB (17.3%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool isvalid(unordered_map<char, int>& m) {
-        if(m['a'] > 0 && m['b'] > 0 && m['c'] > 0) return true;
-        else return false;
+    int solve(string s){
+        int l = 0, r = -1, n = s.length(), ans = 0;
+        unordered_map<char, int> m;
+
+        while (l < n) {
+            while (r + 1 < n && (m['a'] == 0 || m['b'] == 0 || m['c'] == 0)) {
+                r++;
+                m[s[r]]++;
+            }
+            if (m['a'] > 0 && m['b'] > 0 && m['c'] > 0)
+                ans += n - r;
+
+            if (r < l) {
+                l++;
+                r = l - 1;
+            } else {
+                m[s[l]]--;
+                l++;
+            }
+        }
+        return ans;
     }
 
     int numberOfSubstrings(string s) {
-        int n = s.length();
-        int l = 0, r = 0;
-        unordered_map<char, int> m;
-        int ans = 0;
-
-        while (r < n) {
-            m[s[r]]++;
-
-            while (isvalid(m)) { 
-                ans += n - r;
-                m[s[l]]--;
-                // if (m[s[l]] == 0) {
-                //     m.erase(s[l]);  // Remove character when count becomes zero
-                // }
-                l++;
-            }
-            r++;
-        }
-        return ans;
+        return solve(s);
     }
 };
