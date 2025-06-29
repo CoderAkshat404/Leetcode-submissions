a1a3ea0ad4e20365bb66be044a366660a665e19a
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-30 00:36:07 +0530

    Time: 139 ms (6.4%), Space: 149.6 MB (5.43%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,29 +1,39 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+typedef tree<
+    pair<int, int>, 
+    null_type, 
+    less<pair<int, int>>, 
+    rb_tree_tag, 
+    tree_order_statistics_node_update
+> pbds;
 
 class MedianFinder {
 public:
     pbds m;
-    int cnt;  
+    int cnt;
 
     MedianFinder() {
         cnt = 0;
     }
 
     void addNum(int num) {
-        m.insert({num, cnt++});  
+        m.insert({num, cnt++});
     }
 
     double findMedian() {
         int n = m.size();
         if (n & 1) {
-            return (double)(m.find_by_order((n - 1) / 2)->first);
+            // odd size: middle element
+            pair<int, int> p = *m.find_by_order(n / 2);
+            return (double)p.first;
         } else {
-            int x = m.find_by_order(n / 2)->first;
-            int y = m.find_by_order(n / 2 - 1)->first;
-            return ((double)(x + y)) / 2;
+            // even size: average of two middle elements
+            pair<int, int> p1 = *m.find_by_order(n / 2 - 1);
+            pair<int, int> p2 = *m.find_by_order(n / 2);
+            return (p1.first + p2.first) / 2.0;
         }
     }
 };
