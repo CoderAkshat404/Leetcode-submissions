c1997ea15c4142a44de7dfdac15f0b7fb10b030d
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-06-30 14:56:58 +0530

    Time: 58 ms (60.99%), Space: 124.8 MB (95.81%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,39 +1,34 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
-typedef tree<
-    pair<int, int>, 
-    null_type, 
-    less<pair<int, int>>, 
-    rb_tree_tag, 
-    tree_order_statistics_node_update
-> pbds;
-
 class MedianFinder {
 public:
-    pbds m;
-    int cnt;
-
+    priority_queue<int> maxheap;
+    priority_queue<int,vector<int>,greater<int>> minheap;
     MedianFinder() {
-        cnt = 0;
+        
     }
-
+    
     void addNum(int num) {
-        m.insert({num, cnt++});
-    }
-
-    double findMedian() {
-        int n = m.size();
-        if (n & 1) {
-            // odd size: middle element
-            pair<int, int> p = *m.find_by_order(n / 2);
-            return (double)p.first;
-        } else {
-            // even size: average of two middle elements
-            pair<int, int> p1 = *m.find_by_order(n / 2 - 1);
-            pair<int, int> p2 = *m.find_by_order(n / 2);
-            return (p1.first + p2.first) / 2.0;
+        maxheap.push(num);
+        minheap.push(maxheap.top());
+        maxheap.pop();
+        if(minheap.size()>maxheap.size()){
+            maxheap.push(minheap.top());
+            minheap.pop();
         }
+        
+    }
+    
+    double findMedian() {
+        if(minheap.size()==maxheap.size()){
+            return ((double)(minheap.top()+maxheap.top())/2);
+        }
+        return ((double)(maxheap.top()));
+        
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
