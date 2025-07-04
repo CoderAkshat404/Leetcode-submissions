104a5131627cec443e839c1853478b57d75656ce
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-04 18:37:13 +0530

    Time: 1754 ms (19.36%), Space: 9.4 MB (63.88%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,22 +10,21 @@ public:
     }
 
     int solve(int i, int n, string& s, vector<int>& dp) {
-        if (i == n) return 0;
+        if (i == n) return -1;
         if (dp[i] != -1) return dp[i];
 
-        int ans = INT_MAX;
+        int mini = INT_MAX;
         for (int j = i; j < n; j++) {
             if (isPalindrome(s, i, j)) {
-                int cuts = 1 + solve(j + 1, n, s, dp);
-                ans = min(ans, cuts);
+                mini = min(mini, 1 + solve(j + 1, n, s, dp));
             }
         }
-        return dp[i] = ans;
+        return dp[i] = mini;
     }
 
     int minCut(string s) {
         int n = s.length();
         vector<int> dp(n, -1);
-        return solve(0, n, s, dp) - 1; 
+        return solve(0, n, s, dp);
     }
 };
