0c1f9f1232ff1b10e3ad800c309c4ef8a127d817
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-06 16:42:05 +0530

    Time: 3 ms (70.96%), Space: 27.3 MB (75.34%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,23 +11,30 @@
  */
 class Solution {
 public:
-TreeNode* solve(int poss,int pose,int ins,int ine,vector<int>& postorder, vector<int>& inorder,unordered_map<int,int>&m){
-        if (poss > pose || ins > ine) {
-            return nullptr;
+    unordered_map<int,int> m;
+    TreeNode* solve(int ins,int inend,int poss,int posend,vector<int>& inorder, vector<int>& postorder){
+        if(ins>inend){
+            return NULL;
+        }  
+        if(ins==inend){
+            TreeNode* x=new TreeNode(inorder[ins]);
+            return x;
         }
-        TreeNode* x=new TreeNode(postorder[pose]);
-        int numsright=ine-m[x->val];
-        x->left=solve(poss,pose-numsright-1,ins,m[x->val]-1,postorder,inorder,m);
-        x->right=solve(pose-numsright,pose-1,m[x->val]+1,ine,postorder,inorder,m);
-        return x;
+        TreeNode* root=new TreeNode(postorder[posend]);
+        int idx=m[postorder[posend]];
+        root->left = solve(ins, idx - 1, poss, poss + idx - ins - 1, inorder, postorder);
+
+        root->right=solve(idx+1,inend,poss+(idx-ins),posend-1,inorder,postorder);
+        return root;
+
 
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        unordered_map<int,int> m;
-        for(int i=0;i<postorder.size();i++){
+        m.clear();  
+        for(int i=0;i<inorder.size();i++){
             m[inorder[i]]=i;
         }
-        return solve(0,postorder.size()-1,0,inorder.size()-1,postorder,inorder,m);
+        return solve(0,inorder.size()-1,0,postorder.size()-1,inorder,postorder);
         
     }
 };
