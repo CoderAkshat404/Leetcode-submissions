d1771f55633bf21979d3203d835112e6e960515b
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-07 16:39:08 +0530

    Time: 23 ms (88.93%), Space: 26.7 MB (71.59%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s, string t) {
-    int n=s.length();
-	int m=t.length();
-	vector<vector<int>> dp(s.length()+1,vector<int>(t.length()+1,0));
-	for(int i=n-1;i>=0;i--){
-		for(int j=m-1;j>=0;j--){
-			if(s[i]==t[j]){
-				dp[i][j]=1+dp[i+1][j+1];
-			}
-			else{
-				dp[i][j]=max(dp[i][j+1],dp[i+1][j]);
-			}
-		}
-	}
-    string ans="";
-    int i=0;
-    int j=0;
-    while(i<n && j<m){
-        if(s[i]==t[j]){
-            ans+=s[i];
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.length();
+        int m=str2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(str1[i]==str2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        string ans="";
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(str1[i]==str2[j]){
+                ans+=str1[i];
+                i++;
+                j++;
+            }
+            else if(dp[i+1][j]>dp[i][j+1]){
+                ans+=str1[i];
+                i++;
+            }
+            else{
+                ans+=str2[j];
+                j++;
+            }
+        }
+        while(i<n){
+            ans+=str1[i];
             i++;
+        }
+        while(j<m){
+            ans+=str2[j];
             j++;
         }
-        else if(dp[i+1][j]>dp[i][j+1]){
-            ans+=s[i];
-            i++;
-        }
-        else{
-            ans+=t[j];
-            j++;
-        }
-    }
-    while(i<n){
-        ans+=s[i];
-        i++;
-    }
-    while(j<m){
-        ans+=t[j];
-        j++;
-    }
-    return ans;
-	
+        return ans;
         
     }
 };
