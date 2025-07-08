f2c84ad5f3a43a8706e134f483b2e13149bb273e
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-09 01:01:47 +0530

    Time: 2685 ms (8.44%), Space: 68.2 MB (72.73%) - LeetHub

---
+++ 3487-find-maximum-removals-from-source-string/3487-find-maximum-removals-from-source-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    set<int> s;
+    int dp[3001][3001];
+    int solve(int i,int j,string&source, string&pattern){
+        if(i==source.length()){
+            // cout<<j<<endl;
+            if(j==pattern.length()){
+                return 0;
+            }
+            else{
+                // cout<<"this"<<endl;
+                return -1e9;
+            }
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MIN;
+        if(s.find(i)!=s.end()){
+            ans=max(ans,1+solve(i+1,j,source,pattern));
+        }
+        if(source[i]==pattern[j])
+            ans=max(ans,solve(i+1,j+1,source,pattern));
+        ans=max(ans,solve(i+1,j,source,pattern));  
+        // cout<<ans<<endl;
+        return dp[i][j]=ans;
+
+    }
+    int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
+        for(auto i:targetIndices){
+            s.insert(i);
+        }
+        // for(auto i:s){
+        //     cout<<i<<" ";
+        // }
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,source,pattern);
+        
+    }
+};
