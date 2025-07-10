01436941e66ae8ffb22b0798c20d4596b252dca6
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-10 13:13:30 +0530

    Time: 3 ms (37.81%), Space: 10.8 MB (38.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> s;
-        for(int i=0;i<(1<<nums.size());i++){
-            vector<int> v;
-            for(int j=0;j<nums.size();j++){
-                if((i>>j)&1){
-                    v.push_back(nums[j]);
-                }
-            }
-            sort(v.begin(),v.end());
-            s.insert(v);
+    set<vector<int>> ans;
+    vector<int> v;
+    void rec(int i,vector<int>& candidates){
+        if(i>=candidates.size()){
+            ans.insert(v);
+            return  ;
+        }
+       
+            v.push_back(candidates[i]);
+            rec(i+1,candidates);
+            v.pop_back();
+        
+        while (i + 1 < candidates.size() && candidates[i] == candidates[i + 1]) {
+            i++;
+        }
+        rec(i+1,candidates);
 
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        rec(0,nums);
+        vector<vector<int>> arr;
+        for(auto i:ans){
+            arr.push_back(i);
         }
-        vector<vector<int>> ans;
-        for(auto i:s){
-            ans.push_back(i);
-        }
-       return ans;
+        return arr;
+
         
     }
 };
