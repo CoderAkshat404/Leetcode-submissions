df86a2dc31bc2de1ac621060782ba207a8c058d5
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-11 23:47:15 +0530

    Time: 10 ms (19.42%), Space: 16.4 MB (13.54%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,62 +1,40 @@
 class Solution {
 public:
-    unordered_map<int,bool>rowCheck;
-    unordered_map<int,bool>upperLeftDiagonalCheck;
-    unordered_map<int,bool>lowerLeftDiagonalCheck;
-    void printSolution(vector<vector<char>>&board,int n,vector<vector<string>>&ans){
-        vector<string> s;
-        for(int i = 0;i<n;i++){
-            string o="";
-            for(int j = 0;j<n;j++){
-                o.push_back(board[i][j]);
-            }
-            s.push_back(o);
-        }
-        ans.push_back(s);
-    }
-    bool isSafe(int row,int col,int n){
-        if(rowCheck[row] == true)
-            return false;
-        if(upperLeftDiagonalCheck[n-1+col-row] == true){
-            return false;
-        }
-        if(lowerLeftDiagonalCheck[row+col] == true){
-            return false;
-        }
-        return true;
-    }
-    void solve(vector<vector<char>>&board,int col,int n,vector<vector<string>> &ans){
-    // Base Case -> When we have placed queens in all the columns then col will go to n
-        if(col >= n){
-            printSolution(board,n,ans);
+    unordered_map<int,int> row;
+    unordered_map<int,int> upleftdiag;
+    unordered_map<int,int> loleftdiag;
+    vector<vector<string>> ans;
+
+    void rec(int col, vector<string> board, int n){
+        if(col == n){
+            ans.push_back(board);
             return;
         }
-        // Just Solve one case 
-        // In every row, place the queen
-        for(int row = 0;row<n;row++){
-            if(isSafe(row,col,n)){
-                // Place it then
-                board[row][col] = 'Q'; // 1 means queen, 0 means no queen
-                // Map modification
-                rowCheck[row] = true;
-                upperLeftDiagonalCheck[n-1+col-row] = true;
-                lowerLeftDiagonalCheck[row+col] = true;
-                // Recursive call for col + 1
-                solve(board,col+1,n,ans);
-                // Backtracking -> Create original state
-                board[row][col] = '.'; // Queen removed 
-                // Map modification -> recreate original state
-                rowCheck[row] = false;
-                upperLeftDiagonalCheck[n-1+col-row] = false;
-                lowerLeftDiagonalCheck[row+col] = false;
+        for(int i = 0; i < n; i++){
+            if(row[i] == 0 && upleftdiag[col + i] == 0 && loleftdiag[n - 1 + (col - i)] == 0){
+                board[i][col] = 'Q';
+                row[i] = 1;
+                upleftdiag[col + i] = 1;
+                loleftdiag[n - 1 + (col - i)] = 1;
+
+                rec(col + 1, board, n);
+
+                board[i][col] = '.';
+                row[i] = 0;
+                upleftdiag[col + i] = 0;
+                loleftdiag[n - 1 + (col - i)] = 0;
             }
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<vector<char>>board(n,vector<char>(n,'.')); 
-        int col = 0;
-        solve(board,col,n,ans);
-    return ans;   
+        row.clear();
+        upleftdiag.clear();
+        loleftdiag.clear();
+        ans.clear();
+
+        vector<string> board(n, string(n, '.'));
+        rec(0, board, n);
+        return ans;
     }
 };
