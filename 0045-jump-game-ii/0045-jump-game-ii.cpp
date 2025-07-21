09b24b5397f1d8ae73d44008380f28af8cd82e65
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-21 11:05:08 +0530

    Time: 232 ms (5.7%), Space: 22.8 MB (5.21%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    
-   long long  int solve(int i,vector<int>& nums,vector<int>&dp){
+    int solve(int i,vector<int>& nums,vector<int>&dp){
         if(i>=nums.size()-1) return 0;
         if(dp[i]!=-1){
             return dp[i];
         }
-       long long  int ans=INT_MAX;
+        int ans=1e9;
         for(int j=i+1;j<=i+nums[i] && j<nums.size();j++){
             if(nums[j]!=0 || j==nums.size()-1){
             ans=min(ans,1+solve(j,nums,dp));}
