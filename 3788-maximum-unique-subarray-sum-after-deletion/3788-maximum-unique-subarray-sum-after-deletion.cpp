50de60838350ee486237e21a6b965136f11e02a6
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-07-25 11:24:56 +0530

    Time: 2 ms (30.41%), Space: 31 MB (5.24%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int maxSum(vector<int>& nums) {
         unordered_map<int,int> m;
-        for(auto i:nums){
-            m[i]++;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        vector<int> v;
+        for(auto i:m){
+            v.push_back(i.first);
+        }
+        sort(v.rbegin(),v.rend());
+        if(v[0]<0){
+                return v[0];
         }
         int ans=0;
-        int neg=0;
-        for(auto i:m){
-            if(i.first>=0){
-                ans+=i.first;
-            }
-            else{
-                neg+=i.second;
-            }
-        }
-        if(neg==nums.size()){
-            sort(nums.begin(),nums.end());
-            return nums.back();
+        for(int i=0;i<v.size();i++){
+            if(v[i]<0){
+            break;}
+            ans+=v[i];
+
         }
         return ans;
+        
     }
 };
