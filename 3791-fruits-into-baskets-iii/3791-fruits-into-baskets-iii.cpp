c3eca8d5f99d0ffed0e9e5ab71797eedb61d011f
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-08-06 19:31:53 +0530

    Time: 604 ms (22.28%), Space: 176.9 MB (93.31%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -12,16 +12,16 @@ public:
         t[idx]=max(t[2*idx],t[2*idx+1]);
     }
     void update(int idx,int l,int r,int pos,int val){
+        if(pos<l || pos>r){
+            return ;
+        }
         if(l==r){
             t[idx]=val;
             return;
         }
         int mid=(l+r)/2;
-        if (pos <= mid) {
-            update(idx << 1, l, mid, pos, val);
-        } else {
-            update(idx << 1 | 1, mid + 1, r, pos, val);
-        }
+        update(idx << 1, l, mid, pos, val);
+        update(idx << 1 | 1, mid + 1, r, pos, val);
         t[idx]=max(t[2*idx],t[2*idx+1]);
     }
     int query(int idx,int l,int r,int left,int right){
