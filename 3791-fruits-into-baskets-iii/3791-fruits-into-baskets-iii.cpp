b29a7c4d1ec3f0e606cefeb3a28914eba00bbf8a
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-08-06 19:02:39 +0530

    Time: 583 ms (25.9%), Space: 176.9 MB (94.15%) - LeetHub

---
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int t[400400];
+    void build(int idx,int l,int r,vector<int>& baskets){
+        if(l==r){
+            t[idx]=baskets[l];
+            return;
+        }
+        int mid=(l+r)/2;
+        build(2*idx,l,mid,baskets);
+        build(2*idx+1,mid+1,r,baskets);
+        t[idx]=max(t[2*idx],t[2*idx+1]);
+    }
+    void update(int idx,int l,int r,int pos,int val){
+        if(l==r){
+            t[idx]=val;
+            return;
+        }
+        int mid=(l+r)/2;
+        if (pos <= mid) {
+            update(idx << 1, l, mid, pos, val);
+        } else {
+            update(idx << 1 | 1, mid + 1, r, pos, val);
+        }
+        t[idx]=max(t[2*idx],t[2*idx+1]);
+    }
+    int query(int idx,int l,int r,int left,int right){
+        if(left>r || right<l){
+            return INT_MIN;
+        }
+        if(left<=l && r<=right){
+            return t[idx];
+        }
+        int mid=(l+r)/2;
+        return max(query(2*idx,l,mid,left,right),query(2*idx+1,mid+1,r,left,right));
+
+    }
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n=baskets.size();
+        build(1,0,n-1,baskets);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int lo=0;
+            int hi=n-1;
+            int def=-1;
+            while(lo<=hi){
+                int mid=(lo+hi)/2;
+                if(query(1,0,n-1,0,mid)>=fruits[i]){
+                    def=mid;
+                    hi=mid-1;
+                }
+                else{
+                    lo=mid+1;
+                }
+            }
+            if(def!=-1 && baskets[def]>=fruits[i]){
+                update(1,0,n-1,def,INT_MIN);
+            }
+            else{
+                ans++;
+            }
+
+        }
+        return ans;
+        
+    }
+};
