d855d05f5fe776fe55ee92e490e0662b0b95a155
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-08-16 22:31:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (6.32%) - LeetHub

---
+++ 3945-minimum-sensors-to-cover-grid/3945-minimum-sensors-to-cover-grid.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minSensors(int n, int m, int k) {
+        int rows=ceil((double)n/(2*k+1));
+        int cols=ceil((double)m/(2*k+1));
+        return rows*cols;
+        
+    }
+};
