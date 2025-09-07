6ea8ee67f0bf87b7842b9d1d28872aa348e3bbb6
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-09-07 14:16:12 +0530

    Time: 0 ms (100%), Space: 31.4 MB (8.33%) - LeetHub

---
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        set<int> s;
+        for(auto i:nums){
+            s.insert(i);
+        }
+        if(s.size()==1){
+            return 0;
+        }
+        return 1;
+        
+    }
+};
