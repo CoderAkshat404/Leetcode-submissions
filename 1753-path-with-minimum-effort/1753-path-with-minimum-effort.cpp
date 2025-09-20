6731728274524c8e14dadf833a11d7979c82d251
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-09-20 21:00:16 +0530

    Time: 124 ms (21.12%), Space: 38.5 MB (24%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,54 +1,43 @@
 class Solution {
 public:
+    bool check(int i,int j,vector<vector<int>>& grid){
+        return (i>=0) && (i<grid.size()) && (j>=0) && (j<grid[0].size());
+    }
+
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-         priority_queue<pair<int, pair<int, int>>, 
-                   vector<pair<int, pair<int, int>>>, 
-                   greater<pair<int, pair<int, int>>>> q;
-        vector<vector<int>> efforts(n,vector<int>(m,1e9));
-        efforts[0][0]=0;
-        q.push({0,{0,0}});
-        while(!q.empty()){
-            int d=q.top().first;
-            int r=q.top().second.first;
-            int c=q.top().second.second;
-            q.pop();
-            if(r+1<n){
-                int diff=abs(heights[r+1][c]-heights[r][c]);
-                int nd=max(diff,d);
-                if(nd<efforts[r+1][c]){
-                    q.push({nd,{r+1,c}});
-                    efforts[r+1][c]=nd;
-                }
-            }
-            if(c+1<m){
-                int diff=abs(heights[r][c+1]-heights[r][c]);
-                int nd=max(diff,d);
-                if(nd<efforts[r][c+1]){
-                    q.push({nd,{r,c+1}});
-                    efforts[r][c+1]=nd;
-                }
+        int dx[]={1,0,-1,0};
+        int dy[]={0,1,0,-1};
 
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+
+        pq.push({0,0,0});
+        dist[0][0] = 0;
+
+        while(!pq.empty()){
+            int d = pq.top()[0];
+            int i = pq.top()[1];
+            int j = pq.top()[2];
+            pq.pop();
+
+            if(i==n-1 && j==m-1){
+                return d;
             }
-            if(r-1>=0){
-                int diff=abs(heights[r-1][c]-heights[r][c]);
-                int nd=max(diff,d);
-                if(nd<efforts[r-1][c]){
-                    q.push({nd,{r-1,c}});
-                    efforts[r-1][c]=nd;
-                }
-            }
-            if(c-1>=0){
-                int diff=abs(heights[r][c-1]-heights[r][c]);
-                int nd=max(diff,d);
-                if(nd<efforts[r][c-1]){
-                    q.push({nd,{r,c-1}});
-                    efforts[r][c-1]=nd;
+
+            for(int k=0;k<4;k++){
+                int ni=i+dx[k];
+                int nj=j+dy[k];
+                if(check(ni,nj,heights)){
+                    int nd = max(d, abs(heights[ni][nj]-heights[i][j]));
+                    if(nd < dist[ni][nj]){
+                        dist[ni][nj] = nd;
+                        pq.push({nd,ni,nj});
+                    }
                 }
             }
         }
-        return efforts[n-1][m-1];
-        
+        return -1;
     }
 };
