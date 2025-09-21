3b183eeb97b0204b118ddfb3f47c8aacf151b7ff
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-09-21 22:08:39 +0530

    Time: 0 ms (100%), Space: 8 MB (97.11%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length()!=goal.length()){
-            return false;
-        }
-        else{
-            bool found=false;
-            for(int i=0;i<goal.length();i++){
-                string p="";
-                int k=0;
-                for(int j=i;j<goal.length();j++){
-                    k++;
-                    if(k==goal.length()+1){
-                        break;
-                    }
-                    p+=s[j];
-                    cout<<p<<endl;
-                    if(p==goal){
-                        found=true;
-                        break;
-                    }
-                    if(j==goal.length()-1){
-                        j=-1;
-                    }
-       
-                }
-            }
-            if(found){
+        for(int i=0;i<s.length();i++){
+            char c=s.back();
+            s.pop_back();
+            s.insert(s.begin(),c);
+            if(s==goal){
                 return true;
             }
-            else{
-                return false;
-            }
         }
+        return false;
         
     }
 };
