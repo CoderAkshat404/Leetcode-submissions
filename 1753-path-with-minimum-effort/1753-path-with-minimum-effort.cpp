c32fb0b83f31d4331207c218033eba992dc32e0f
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-09-26 18:50:54 +0530

    Time: 60 ms (44.03%), Space: 24.5 MB (54.65%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    bool check(int i,int j,vector<vector<int>>& grid){
-        return (i>=0) && (i<grid.size()) && (j>=0) && (j<grid[0].size());
+    bool isValid(int r,int c,int rows,int cols){
+        return (r>=0 && c>=0 && r<rows && c<cols);
     }
-
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
-        int dx[]={1,0,-1,0};
-        int dy[]={0,1,0,-1};
-
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-
-        pq.push({0,0,0});
-        dist[0][0] = 0;
-
+        int rows = heights.size(), cols = heights[0].size();
+        vector<vector<int>> minEffort(rows,vector<int>(cols,1e9));
+        minEffort[0][0] = 0;
+        priority_queue<
+        pair<int,pair<int,int>>,//effort,x,y
+        vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>
+        > pq;
+        pq.push({0,{0,0}});
+        int dr[] = {0,1,0,-1};
+        int dc[] = {1,0,-1,0};
         while(!pq.empty()){
-            int d = pq.top()[0];
-            int i = pq.top()[1];
-            int j = pq.top()[2];
+            int eff = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
-
-            if(i==n-1 && j==m-1){
-                return d;
-            }
-
-            for(int k=0;k<4;k++){
-                int ni=i+dx[k];
-                int nj=j+dy[k];
-                if(check(ni,nj,heights)){
-                    int nd = max(d, abs(heights[ni][nj]-heights[i][j]));
-                    if(nd < dist[ni][nj]){
-                        dist[ni][nj] = nd;
-                        pq.push({nd,ni,nj});
+            for(int i=0;i<4;i++){
+                int nr = r + dr[i], nc = c + dc[i];
+                if(isValid(nr,nc,rows,cols)){
+                    int newEff = abs(heights[r][c]-heights[nr][nc]);
+                    if(max(eff,newEff) < minEffort[nr][nc]){
+                        minEffort[nr][nc] = max(eff,newEff);
+                        pq.push({minEffort[nr][nc],{nr,nc}});
                     }
                 }
             }
         }
-        return -1;
+        return minEffort[rows-1][cols-1];
     }
 };
