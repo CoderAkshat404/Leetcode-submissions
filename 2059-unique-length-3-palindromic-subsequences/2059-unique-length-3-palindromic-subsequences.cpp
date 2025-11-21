c19126fd3da78e9c05b877b0312befe515eb0b74
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2025-11-21 18:27:48 +0530

    Time: 389 ms (17.53%), Space: 16.8 MB (23.82%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_map<char,int> m1;
-        unordered_map<char,bool> mp1;
-        unordered_map<char,int> m2;
+        map<char,int> f;
+        map<char,int> l;
+        map<char,int> count;
         for(int i=0;i<s.length();i++){
-            if(mp1[s[i]]==false){
-                m1[s[i]]=i;
-                mp1[s[i]]=true;
+            if(f.find(s[i])==f.end()){
+                f[s[i]]=i;
             }
-        }
-        unordered_map<char,bool> mp2;
-        for(int i=s.length()-1;i>=0;i--){
-            if(mp2[s[i]]==false){
-                m2[s[i]]=i;
-                mp2[s[i]]=true;
-            }
+            l[s[i]]=i;
+            count[s[i]]++;
         }
         int ans=0;
-        for(auto i:m1){
-            if(i.second==m2[i.first]){
-                ans+=0;
-            }
-            else{
-                unordered_map<char,bool> m3;
-                for(int j=i.second+1;j<m2[i.first];j++){
-                    if(m3[s[j]]==false){
-                        m3[s[j]]=true;
-                        ans++;
-                    }
+        for(auto i:f){
+            int x=i.second;
+            int y=l[i.first];
+            set<char> a;
+            if(x!=y){
+                for(int j=x+1;j<y;j++){
+                    a.insert(s[j]);
                 }
+                ans+=a.size();
             }
         }
         return ans;
-
-
         
     }
 };
