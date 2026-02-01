308a968233e32fc1b05e4242cf520cca5f0e5e66
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-02-01 20:08:46 +0530

    Time: 94 ms (5.04%), Space: 152.6 MB (21.01%) - LeetHub

---
+++ 3829-design-ride-sharing-system/3829-design-ride-sharing-system.cpp
@@ -0,0 +1,58 @@
+class RideSharingSystem {
+public:
+    map<int,int> m;
+    set<pair<int,int>> driver;
+    set<pair<int,int>> rider;
+    int cntr=0;
+    int cntd=0;
+
+    RideSharingSystem() {
+        
+    }
+    
+    void addRider(int riderId) {
+        rider.insert({cntr,riderId});
+        m[riderId]=cntr;
+        cntr++;
+        
+    }
+    
+    void addDriver(int driverId) {
+        driver.insert({cntd,driverId});
+        cntd++;
+        
+    }
+    
+    vector<int> matchDriverWithRider() {
+        if(!rider.empty() && !driver.empty()){
+            int riderId=(*rider.begin()).second;
+            int driverId=(*driver.begin()).second;
+            rider.erase(rider.begin());
+            driver.erase(driver.begin());
+            return {driverId,riderId};
+
+        }
+        return {-1,-1};
+        
+    }
+    
+    void cancelRider(int riderId) {
+        int cntr=m[riderId];
+        pair<int,int> p={cntr,riderId};
+        auto it = rider.find(p);
+        if (it != rider.end()) {
+            rider.erase(it);
+        }
+        
+        
+    }
+};
+
+/**
+ * Your RideSharingSystem object will be instantiated and called as such:
+ * RideSharingSystem* obj = new RideSharingSystem();
+ * obj->addRider(riderId);
+ * obj->addDriver(driverId);
+ * vector<int> param_3 = obj->matchDriverWithRider();
+ * obj->cancelRider(riderId);
+ */
