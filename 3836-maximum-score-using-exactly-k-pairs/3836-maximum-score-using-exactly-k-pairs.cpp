df1749d5b1115b7114808b523a374ce8042a1f73
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-02-18 00:03:23 +0530

    Time: 699 ms (71.69%), Space: 57.6 MB (91.14%) - LeetHub

---
+++ 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
@@ -0,0 +1,33 @@
+#define ll long long
+class Solution {
+public:
+    ll dp[101][101][101];
+    ll solve(int i,int j,vector<int>& nums1, vector<int>& nums2,int k){
+        if(k==0) return 0;
+
+        if(nums1.size()-i < k || nums2.size()-j < k)
+             return -1e18;
+        if(k<0){
+            return -1e18;
+        }
+        if(dp[i][j][k]!=-1e18){
+            return dp[i][j][k];
+        }
+        ll ans=-1e18;
+        ans=max(ans,(1LL*nums1[i]*nums2[j])+solve(i+1,j+1,nums1,nums2,k-1));
+        ans=max(ans,solve(i+1,j,nums1,nums2,k));
+        ans=max(ans,solve(i,j+1,nums1,nums2,k));
+        return dp[i][j][k]=ans;
+    }
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                for(int k=0;k<101;k++){
+                    dp[i][j][k]=-1e18;
+                }
+            }
+        }
+        return solve(0,0,nums1,nums2,k);    
+        
+    }
+};
