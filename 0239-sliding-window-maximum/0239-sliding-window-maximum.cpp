1c779d52986ca402a94d96c14d57005bcdddad6a
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-03-30 23:13:33 +0530

    Time: 23 ms (59.71%), Space: 139.3 MB (25.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+    struct datas{
+        deque<int> dq;
+        void insert(int n){
+            while(!dq.empty() && dq.back()<n){
+                dq.pop_back();
+            }
+            dq.push_back(n);
+        }
+        void rem(int n){
+            if(dq.front()==n){
+                dq.pop_front();
+            }
+        }
+        int getmax(){
+            return  dq.front();
+        }
+    };
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        multiset<int> m;
+        datas ds;
         vector<int> ans;
         for(int i=0;i<k;i++){
-            m.insert(nums[i]);
+            ds.insert(nums[i]);
         }
-        ans.push_back(*m.rbegin());
+        ans.push_back(ds.getmax());
         int i=0;
         int j=k-1;
-        while(j+1<n){
-            m.insert(nums[j+1]);
-            m.erase(m.find(nums[i]));
-             ans.push_back(*m.rbegin());
-             i++;
-             j++;
-
+        while(j+1<nums.size()){
+            ds.insert(nums[j+1]);
+            ds.rem(nums[i]);
+            ans.push_back(ds.getmax());
+            i++;
+            j++;
         }
         return ans;
         
