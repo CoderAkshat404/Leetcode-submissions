bf6673efc16da1cf755c622d610a2b90e9fdcb53
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-04-03 22:25:16 +0530

    Time: 0 ms (100%), Space: 95 MB (94.32%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        for(int i=0;i<nums2.size();i++){
-            nums1.push_back(nums2[i]);
+    int check(int mid,int k,vector<int>& nums1, vector<int>& nums2){
+        int tot=0;
+        tot+=upper_bound(nums1.begin(),nums1.end(),mid)-nums1.begin();
+        tot+=upper_bound(nums2.begin(),nums2.end(),mid)-nums2.begin();
+        return tot>=k;
+    }
+    int findkth(int k,vector<int>& nums1, vector<int>& nums2){
+        int lo=-1e6;
+        int hi=1e6;
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(check(mid,k,nums1,nums2)){
+                ans=mid;
+                hi=mid-1;
 
+            }
+            else{
+                lo=mid+1;
+            }
         }
-        sort(nums1.begin(),nums1.end());
-        if(nums1.size()&1){
-            return nums1[nums1.size()/2];
+        return ans;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        double ans=0;
+        if((n+m)%2==1){
+            ans=findkth((n+m+1)/2,nums1,nums2);
+            
         }
         else{
-            float x=(float)(nums1[nums1.size()/2]+nums1[nums1.size()/2-1])/2;
-            return x;
+            ans+=findkth((n+m)/2,nums1,nums2);
+            ans+=findkth((n+m+2)/2,nums1,nums2);
+            ans/=2.0;
+
         }
+        return ans;
+        
         
     }
 };
