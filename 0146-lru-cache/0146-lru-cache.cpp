38f5f4416c4f1388964c812508ae5e36c6bf601a
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-07-19 11:49:27 +0530

    Time: 147 ms (5.12%), Space: 183.8 MB (21.77%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,81 +1,47 @@
-class Node{
-    public:
-    int key;
-    int value;
-    Node* prev;
-    Node* next;
-    Node(int key,int value){
-        this->key=key;
-        this->value=value;
-        prev=NULL;
-        next=NULL;
-    }
-
-};
 class LRUCache {
 public:
-    int capa;
-    unordered_map<int,Node*> m;
-    Node *head;
-    Node *tail;
-    void insertAfterHead(Node *node){
-        Node *nodeja=head->next;
-        head->next=node;
-        node->prev=head;
-        node->next=nodeja;
-        nodeja->prev=node;
-    }
-    void deletenode(Node* node){
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
-        node->prev=NULL;
-        node->next=NULL;
-    }
+    int capacity;
+    list<int> dll;
+    map<int,pair<int,list<int>::iterator>> m;
+    void touch(int x){
+        auto it=m[x].second;
+        dll.erase(it);
+        dll.push_front(x);
+        it=dll.begin();
+        m[x].second=it;
 
-
+    }
     LRUCache(int capacity) {
-        capa=capacity;
-        head=new Node(1,1);
-        tail=new Node(1,1);
-        head->next=tail;
-        tail->prev=head;
+        this->capacity=capacity;
         
     }
     
     int get(int key) {
-        if(m.find(key)==m.end()){
-            return -1;
-        }
-        Node* node=m[key];
-        deletenode(node);
-        insertAfterHead(node);
-        return node->value;
+        if(m.find(key)==m.end()) return -1;
+        int x=m[key].first;
+        touch(key);
+        return x;
         
     }
     
     void put(int key, int value) {
-        if(m.find(key)==m.end()){
-            if(m.size()==capa){
-                Node* node=tail->prev;
-                m.erase(node->key);
-                deletenode(node);
-                delete node;
-                Node* node2=new Node(key,value);
-                m[key]=node2;
-                insertAfterHead(node2);
-                
-            }
-            else{
-            Node* node2 = new Node(key, value);
-            m[key] = node2;
-            insertAfterHead(node2);}
-
+        if(m.find(key)!=m.end()){
+            m[key].first=value;
+            touch(key);
         }
-        Node* node=m[key];
-        node->value=value;
-        deletenode(node);
-        insertAfterHead(node);
-        
+        else if(dll.size()==capacity){
+            int last=dll.back();
+            dll.pop_back();
+            m.erase(last);
+            dll.push_front(key);
+            auto it=dll.begin();
+            m[key]={value,it};
+        }
+        else{
+            dll.push_front(key);
+            auto it=dll.begin();
+            m[key]={value,it};
+        }
         
     }
 };
