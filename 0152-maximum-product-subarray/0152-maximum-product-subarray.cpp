8c253c7f9e0df6618c779e5c75c7aa4a4fbc6e80
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-07-29 19:22:07 +0530

    Time: 0 ms (100%), Space: 17.6 MB (87.43%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        set<int> pos;
-        set<int> neg;
-        pos.insert(1);
-        neg.insert(1);
-        int pro=1;
-        int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            // cout<<pos.size()<<" "<<neg.size()<<endl;
-            pro=pro*nums[i];
-            if(pro>=0){
-                maxi=max(maxi,(pro/(*pos.begin())));
-            }
-            else{
-                maxi=max(maxi,(pro/(*neg.rbegin())));
-            }
-            maxi=max(maxi,pro);
-            if(pro>0){
-                pos.insert(pro);
-            }
-            if(pro<0){
-                neg.insert(pro);
-                neg.erase(1);
-            }
-            if(pro==0){
-                pro=1;
-                pos.clear();
-                neg.clear();
-                pos.insert(1);
-                neg.insert(1);
-            }
+    int maxProduct(vector<int>& arr) {
+        int n=arr.size();
+        int pre=1;
+        int suff=1;
+        int ans=-1e9;
+        for (int i = 0; i < n; i++) {
+            pre *= arr[i];
+            suff *= arr[n - i - 1];
+            ans = max(ans, max(pre, suff));
+             if (pre == 0) pre = 1;
+            if (suff == 0) suff = 1;
         }
-        return maxi;
+        return ans;
         
     }
 };
