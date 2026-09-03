6093bb946cafe77467cb606cbe47daf83d60f5d6
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-09-03 10:40:03 +0530

    Time: 0 ms (100%), Space: 165.8 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
-        bool flag1=true;
-        multiset<int> ms;
-        for(auto i:nums1){
-            if(i&1){
-                ms.insert(i);
-            }
+    bool uniformArray(vector<int>& nums) {
+        int oddc=0;
+        int evenc=0;
+        int mini=1e9;
+        for(auto i:nums){
+            if(i&1) oddc++;
+            else evenc++;
+            mini=min(mini,i);
         }
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[i]%2==0){
-                if(nums1[i]-(*ms.begin())<1){
-                    flag1=false;
-                    break;
-                }
-            }
-        }
-        bool flag2=true;
-       
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[i]%2==1){
-                if((*ms.begin()==nums1[i])){
-                     flag2=false;
-                    break;
-
-                }
-               
-            }
-            
-        }
-        return (flag1 || flag2);
-
-
+        if(evenc==0) return true;
+        if(oddc==0) return true;
+        return mini%2==1;
         
     }
 };
