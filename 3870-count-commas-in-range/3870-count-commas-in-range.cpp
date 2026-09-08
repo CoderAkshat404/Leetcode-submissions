bbb901a69e8eccc06cd16ff7f3812030df913e36
CoderAkshat404 <148848744+CoderAkshat404@users.noreply.github.com>
2026-09-08 11:20:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int calsize(int n){
-        int x=0;
-        while(n!=0){
-            x++;
-            n=n/10;
-        }
-        return x;
-    }
     int countCommas(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans+=calsize(i)/4;
-        }
-        return ans;
+        if(n<1000) return 0;
+        return n-1000+1;
         
     }
 };
